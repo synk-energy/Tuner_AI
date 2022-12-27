1096b2c0dc55cca120be235e2f331cdddd511612
synk-energy <synk@lo3skunk.works>
2022-12-26 18:54:02 -0800

    Update script.js

--- client/script.js
+++ client/script.js
@@ -79,7 +79,7 @@ loader(messageDiv)
 
 //fetch data from server -> bot's response
 
-const response = await fetch('http://localhost:5000',{
+const response = await fetch('https://boosted-4-0-tunerai.onrender.com',{
 method: 'POST',
 headers: {
   'Content-Type': 'application/json'
